495c63f8870ebe2c3d10adf1730747f88442d277
Junzhou Chen <Junzhou.Chen@med.usc.edu>
2023-05-30 01:55:08 -0700

    default GPU device 0

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ from  display_output import display_output
 parser = parser_ops.get_parser()
 args = parser.parse_args()
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 working_dir = os.environ["HOME"] + '/DL/SSDU'
 
 #..............................................................................
